1bb67ce32dbd41cc8dd6bc7c0edec658246b5748
yairgolo <93384953+yairgolo@users.noreply.github.com>
2022-04-21 00:17:59 +0300

    Update DbConfig.java

--- src/com/company/util/DbConfig.java
+++ src/com/company/util/DbConfig.java
@@ -3,5 +3,5 @@ package com.company.util;
 public class DbConfig {
     public final static String sqlUrl = "jdbc:mysql://localhost:3306/cat-example?useSSL=false&serverTimezone=UTC";
     public final static String sqlUser = "root";
-    public final static String sqlPassword  = "yairgolo10";
+    public final static String sqlPassword  = "password";
 }
